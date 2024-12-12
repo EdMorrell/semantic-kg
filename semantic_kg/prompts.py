72cda10c2a7b64de7cbd4b0a95709eefca64dfd3
Ed Morrell <ed.r.morrell@gsk.com>
2024-12-12 10:30:25 +0000

    Removed primekg-specific instruction

--- semantic_kg/prompts.py
+++ semantic_kg/prompts.py
@@ -4,8 +4,6 @@ Your goal is to convert this list into a natural language statement that is writ
 
 {dataset_rules}
 
-If the relation is described as "parent-child" this refers to the fact that the "source_node" is a sub-type of the "target_node", it does not refer to hereditary relations.
-
 You may use synonyms to descibe the terms if these produce a more natural sounding statement and the statement does not have to follow the same order as the triplets. You are also welcome to add additional contextual information, for example, definitons of the terms involved, however the additional information must not contradict the relations described by the triplets.
 
 Triplets: {triplets}
