e9d7d724a84e3ce2c4fa5c7db69356d5c1648652
Ed Morrell <ed.r.morrell@gsk.com>
2024-12-19 16:15:18 +0000

    Added support for structured generation

--- semantic_kg/models/llm.py
+++ semantic_kg/models/llm.py
@@ -8,6 +8,12 @@ from semantic_kg.models.base import BaseTextGeneration
 from semantic_kg.models.utils import get_hishel_http_client
 
 
+class InvalidResponseError(Exception):
+    """Error class for invalid LLM response"""
+
+    pass
+
+
 def create_azure_openai_client(cache_dir: Optional[Path] = None):
     """Sets up the Azure OpenAI client"""
     # Creates a hishel object to cache responses
@@ -26,7 +32,12 @@ def create_azure_openai_client(cache_dir: Optional[Path] = None):
 
 class OpenAITextGeneration(BaseTextGeneration):
     def __init__(
-        self, model_id: str, cache_dir: Optional[Path] = None, temperature: float = 1.0
+        self,
+        model_id: str,
+        cache_dir: Optional[Path] = None,
+        temperature: float = 1.0,
+        system_prompt: Optional[str] = None,
+        structured_output: bool = False,
     ) -> None:
         """Model class for Azure OpenAI text generation models
 
@@ -38,17 +49,27 @@ class OpenAITextGeneration(BaseTextGeneration):
             Path to use for caching responses. If not provided then responses not cached
         temperature : float, optional
             Temperature for generation, by default 1.0
+        system_prompt : str, optional
+            System prompt to use. If not provided then only user prompt used.
+        structured_output : bool, optional
+            Whether to provide a JSON structured output, defaults to False.
         """
         super().__init__()
         self.model_id = model_id
         self.temperature = temperature
         if not cache_dir:
             print("No cache directory provided, responses will not be cached")
+        self.system_prompt = system_prompt
+        self.structured_output = structured_output
 
         self.client = create_azure_openai_client(cache_dir)
 
     def generate(
-        self, prompt: str, n_responses: int, max_tokens: int
+        self,
+        prompt: str,
+        n_responses: int,
+        max_tokens: int,
+        seed: Optional[int] = None,
     ) -> list[str | None]:
         """Generates a list of responses to a prompt
 
@@ -60,17 +81,35 @@ class OpenAITextGeneration(BaseTextGeneration):
             Number of responses to generate
         max_tokens : int
             Max number of tokens per response
+        seed : Optional[int], optional
+            Random seed for generation
 
         Returns
         -------
         list[str | None]
             List of generated responses
         """
+        if self.system_prompt:
+            messages = [
+                {"role": "system", "content": self.system_prompt},
+                {"role": "user", "content": prompt},
+            ]
+        else:
+            messages = [{"role": "user", "content": prompt}]
+
+        kwargs = {}
+        if self.structured_output:
+            kwargs["response_format"] = {"type": "json_object"}
+
+        if seed:
+            kwargs["seed"] = seed
+
         response = self.client.chat.completions.create(
             model=self.model_id,
             temperature=1,
             max_tokens=max_tokens,
             n=n_responses,
-            messages=[{"role": "user", "content": prompt}],
+            messages=messages,  # type: ignore
+            **kwargs,
         )
         return [choice.message.content for choice in response.choices]
