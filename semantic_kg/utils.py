cb4206e951cde87b92af8a99ab28084670b83dd5
Ed Morrell <ed.r.morrell@gsk.com>
2025-01-10 17:40:08 +0000

    Moved get_hishel_client function here

--- semantic_kg/utils.py
+++ semantic_kg/utils.py
@@ -1,4 +1,8 @@
+from typing import Optional
+from pathlib import Path
 import string
+
+import hishel
 import numpy as np
 import networkx as nx
 
@@ -21,3 +25,15 @@ def find_field_placeholders(text: str) -> list[str]:
     """
     placeholders = [item[1] for item in string.Formatter().parse(text) if item[1]]
     return placeholders
+
+
+def get_hishel_http_client(cache_dir: Optional[str | Path] = None):
+    """Wraps an HTTP client to enable caching"""
+    if isinstance(cache_dir, str):
+        cache_dir = Path(cache_dir)
+
+    controller = hishel.Controller(force_cache=True, cacheable_methods=["GET", "POST"])
+    storage = hishel.FileStorage(base_path=cache_dir)
+    http_client = hishel.CacheClient(controller=controller, storage=storage)
+
+    return http_client
