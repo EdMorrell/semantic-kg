333b22ccfa749a150f453c1555bd2e53a44ceb8c
Ed Morrell <ed.r.morrell@gsk.com>
2025-02-03 15:23:29 +0000

    Modified loader to make a class

--- semantic_kg/datasets/prime_kg.py
+++ semantic_kg/datasets/prime_kg.py
@@ -1,57 +1,66 @@
+from pathlib import Path
 import random
 
 import pandas as pd
 
+from semantic_kg.datasets import BaseDatasetLoader
 
-def load_triple_df(node_fpath: str, edge_fpath: str) -> pd.DataFrame:
-    node_df = pd.read_csv(node_fpath)
-    edge_df = pd.read_csv(edge_fpath)
 
-    triple_df = edge_df.join(node_df, on="x_index", rsuffix="x").join(
-        node_df, on="y_index", rsuffix="_y"
-    )
+class PrimeKGLoader(BaseDatasetLoader):
+    def __init__(self, data_dir: str | Path) -> None:
+        super().__init__(data_dir=data_dir)
+        self.node_fpath = self.data_dir / "nodes.csv"
+        if not self.node_fpath.exists():
+            raise ValueError(f"{self.node_fpath} does not exist")
+        self.edge_fpath = self.data_dir / "edges.csv"
+        if not self.edge_fpath.exists():
+            raise ValueError(f"{self.edge_fpath} does not exist")
 
-    triple_df = triple_df.rename(
-        {
-            "node_index": "src_node_index",
-            "node_type": "src_node_type",
-            "node_name": "src_node_name",
-            "node_index_y": "target_node_index",
-            "node_type_y": "target_node_type",
-            "node_name_y": "target_node_name",
-        },
-        axis=1,
-    )
+    def load(self) -> pd.DataFrame:
+        node_df = pd.read_csv(self.node_fpath)
+        edge_df = pd.read_csv(self.edge_fpath)
 
-    return triple_df[
-        [
-            "src_node_index",
-            "src_node_type",
-            "src_node_name",
-            "display_relation",
-            "target_node_index",
-            "target_node_type",
-            "target_node_name",
+        triple_df = edge_df.join(node_df, on="x_index", rsuffix="x").join(
+            node_df, on="y_index", rsuffix="_y"
+        )
+
+        triple_df = triple_df.rename(
+            {
+                "node_index": "src_node_index",
+                "node_type": "src_node_type",
+                "node_name": "src_node_name",
+                "node_index_y": "target_node_index",
+                "node_type_y": "target_node_type",
+                "node_name_y": "target_node_name",
+            },
+            axis=1,
+        )
+
+        return triple_df[
+            [
+                "src_node_index",
+                "src_node_type",
+                "src_node_name",
+                "display_relation",
+                "target_node_index",
+                "target_node_type",
+                "target_node_name",
+            ]
         ]
-    ]
 
 
 if __name__ == "__main__":
     import numpy as np
 
-    from semantic_kg.sampling import SubgraphSampler, SubgraphDataset
-    from semantic_kg.perturbation import build_perturber
-    from semantic_kg.datasets import KGLoader, create_edge_map, get_valid_node_pairs
+    from semantic_kg.generation import SubgraphPipeline
 
     random.seed(42)
     np.random.seed(42)
 
-    node_fpath = "datasets/prime_kg/nodes.csv"
-    edge_fpath = "datasets/prime_kg/edges.csv"
+    primekg_loader = PrimeKGLoader("datasets/prime_kg")
+    df = primekg_loader.load()
 
-    df = load_triple_df(node_fpath, edge_fpath)
-
-    kg_loader = KGLoader(
+    pipeline = SubgraphPipeline(
         src_node_id_field="src_node_index",
         src_node_type_field="src_node_type",
         src_node_name_field="src_node_name",
@@ -59,37 +68,6 @@ if __name__ == "__main__":
         target_node_id_field="target_node_index",
         target_node_type_field="target_node_type",
         target_node_name_field="target_node_name",
-    )
-    g = kg_loader.load(triple_df=df, directed=False)
-
-    edge_map = create_edge_map(
-        df,
-        src_node_type_field="src_node_type",
-        target_node_type_field="target_node_type",
-        edge_name_field="display_relation",
-    )
-    replace_map = {k: v for k, v in edge_map.items() if len(v) > 1}
-    valid_node_pairs = get_valid_node_pairs(
-        df,
-        src_node_type_field="src_node_type",
-        target_node_type_field="target_node_type",
-    )
-
-    perturber = build_perturber(
-        edge_map=edge_map,
-        valid_node_pairs=valid_node_pairs,  # type: ignore
-        replace_map=replace_map,
         directed=False,
-        p_prob=[0.3, 0.3, 0.3, 0.1],
     )
-
-    sampler = SubgraphSampler(graph=g, method="bfs_node_diversity")
-
-    subgraph = SubgraphDataset(
-        graph=g,
-        subgraph_sampler=sampler,
-        perturber=perturber,
-        n_node_range=(3, 10),
-        save_subgraphs=False,
-    )
-    sample_df = subgraph.generate(1000, 10000)
+    subgraph_df = pipeline.generate(df, 1000)
