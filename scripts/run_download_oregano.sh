685efb75f2c96c4bcd84782f1f2d87dedb5d009c
Ed Morrell <ed.r.morrell@gsk.com>
2025-02-13 10:10:31 -0500

    Refactored to avoid repetition

--- scripts/run_download_oregano.sh
+++ scripts/run_download_oregano.sh
@@ -2,11 +2,9 @@
 
 DPATH=${1:-datasets/oregano/}
 
-
 if [ ! -d $DPATH ]; then
     mkdir -p $DPATH
 fi
-
 
 # Function to download a file from url if it does not exist
 download_file() {
@@ -18,39 +16,23 @@ download_file() {
     fi
 }
 
+# Array of URLs and filenames
+files=(
+    "https://ndownloader.figshare.com/files/42612700 $DPATH/OREGANO_V2.1.tsv"
+    "https://ndownloader.figshare.com/files/42700234 $DPATH/oreganov2.1_metadata_complete.ttl"
+    "https://ndownloader.figshare.com/files/42612697 $DPATH/ACTIVITY.tsv"
+    "https://ndownloader.figshare.com/files/42612676 $DPATH/COMPOUND.tsv"
+    "https://ndownloader.figshare.com/files/42612685 $DPATH/DISEASES.tsv"
+    "https://ndownloader.figshare.com/files/42612694 $DPATH/EFFECT.tsv"
+    "https://ndownloader.figshare.com/files/42612673 $DPATH/GENES.tsv"
+    "https://ndownloader.figshare.com/files/42612691 $DPATH/INDICATION.tsv"
+    "https://ndownloader.figshare.com/files/42612688 $DPATH/PATHWAYS.tsv"
+    "https://ndownloader.figshare.com/files/42612679 $DPATH/PHENOTYPES.tsv"
+    "https://ndownloader.figshare.com/files/42612670 $DPATH/SIDE_EFFECT.tsv"
+    "https://ndownloader.figshare.com/files/42612682 $DPATH/TARGET.tsv"
+)
 
-# Triples
-download_file https://ndownloader.figshare.com/files/42612700 $DPATH/OREGANO_V2.1.tsv
-
-# Metadata
-download_file https://ndownloader.figshare.com/files/42700234 $DPATH/oreganov2.1_metadata_complete.ttl
-
-# Activity
-download_file https://ndownloader.figshare.com/files/42612697 $DPATH/ACTIVITY.tsv
-
-# Compound
-download_file https://ndownloader.figshare.com/files/42612676 $DPATH/COMPOUND.tsv
-
-# Diseases
-download_file https://ndownloader.figshare.com/files/42612685 $DPATH/DISEASES.tsv
-
-# Effects
-download_file https://ndownloader.figshare.com/files/42612694 $DPATH/EFFECT.tsv
-
-# Genes
-download_file https://ndownloader.figshare.com/files/42612673 $DPATH/GENES.tsv
-
-# Indication
-download_file https://ndownloader.figshare.com/files/42612691 $DPATH/INDICATION.tsv
-
-# Pathways
-download_file https://ndownloader.figshare.com/files/42612688 $DPATH/PATHWAYS.tsv
-
-# Phenotype
-download_file https://ndownloader.figshare.com/files/42612679 $DPATH/PHENOTYPES.tsv
-
-# Side-effects
-download_file https://ndownloader.figshare.com/files/42612670 $DPATH/SIDE_EFFECT.tsv
-
-# Targets
-download_file https://ndownloader.figshare.com/files/42612682 $DPATH/TARGET.tsv
+# Download files from the array
+for file in "${files[@]}"; do
+    download_file $file
+done
