b189076f72ca84f811b1b327a07bd2705f73e7e2
Ed Morrell <ed.r.morrell@gsk.com>
2025-04-14 17:21:17 +0100

    Moved prompt loaders into general functions in new utils script

--- semantic_kg/prompts/__init__.py
+++ semantic_kg/prompts/__init__.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 from semantic_kg.prompts import oregano
 from semantic_kg.prompts import prime_kg
+from semantic_kg.prompts import utils
 
 
 class PromptConfig(BaseModel):
@@ -11,14 +12,37 @@ class PromptConfig(BaseModel):
 
 
 OREGANO_PROMPT_CONFIG = PromptConfig(
-    response_model_prompt=oregano.get_default_prompt_template(),
-    entity_extractor_scorer_prompt=oregano.get_entity_extractor_system_prompt(),
-    kg_extractor_scorer_prompt=oregano.get_kg_extractor_system_prompt(),
+    response_model_prompt=utils.get_default_prompt_template(
+        fewshot_examples=oregano.oregano_fewshot_examples,
+        prompt_rules=oregano.oregano_prompt_rules,
+    ),
+    entity_extractor_scorer_prompt=utils.get_entity_extractor_system_prompt(
+        node_types=oregano.NODE_TYPES,
+        entity_extractor_fewshot_examples=oregano.entity_extractor_fewshot_examples,
+    ),
+    kg_extractor_scorer_prompt=utils.get_kg_extractor_system_prompt(
+        edge_types=oregano.EDGE_TYPES,
+        fewshot_examples=oregano.kg_extractor_fewshot_example,
+        directed=True,
+        incl_valid_directions=True,
+        valid_edge_directions=oregano.VALID_EDGE_DIRECTIONS,
+    ),
 )
 
 
 PRIME_KG_PROMPT_CONFIG = PromptConfig(
-    response_model_prompt=prime_kg.get_default_prompt_template(),
-    entity_extractor_scorer_prompt=prime_kg.get_entity_extractor_system_prompt(),
-    kg_extractor_scorer_prompt=prime_kg.get_kg_extractor_system_prompt(),
+    response_model_prompt=utils.get_default_prompt_template(
+        fewshot_examples=prime_kg.prime_kg_fewshot_examples,
+        prompt_rules=prime_kg.prime_kg_prompt_rules,
+        directed=True,
+    ),
+    entity_extractor_scorer_prompt=utils.get_entity_extractor_system_prompt(
+        node_types=prime_kg.NODE_TYPES,
+        entity_extractor_fewshot_examples=prime_kg.entity_extractor_fewshot_examples,
+    ),
+    kg_extractor_scorer_prompt=utils.get_kg_extractor_system_prompt(
+        edge_types=prime_kg.EDGE_TYPES,
+        fewshot_examples=prime_kg.kg_extractor_fewshot_examples,
+        incl_valid_directions=False,
+    ),
 )
