854e3126a7c749fb0292c27574e2ab65a9b57ae4
Ed Morrell <ed.r.morrell@gsk.com>
2025-07-25 11:34:20 -0400

    Added gemini to models

--- semantic_kg/models/__init__.py
+++ semantic_kg/models/__init__.py
@@ -1,8 +1,8 @@
 from semantic_kg.models.base import BaseTextGeneration
-from semantic_kg.models.llm import OpenAITextGeneration
+from semantic_kg.models.llm import OpenAITextGeneration, GeminiTextGeneration
 
 
-MODEL_MAP = {"openai": OpenAITextGeneration}
+MODEL_MAP = {"openai": OpenAITextGeneration, "gemini": GeminiTextGeneration}
 
 
 def load_model(model_name: str, **kwargs) -> BaseTextGeneration:
